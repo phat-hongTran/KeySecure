d592a17a9a15a4b932c17bf61f7870f9fd59092f
huypham0808 <phamngohoanghuy95@gmail.com>
2023-11-04 22:13:25 +0700

    Intergrate material design lib and implement encrypt interface

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace KeySecure.ViewModels
+{
+    public class ViewModelBase
+    {
+        public class ToggleButtonVisibilityConverter : IValueConverter
+        {
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                bool isChecked = (bool)value;
+                return isChecked ? Visibility.Collapsed : Visibility.Visible;
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        public class ToggleButtonInverseVisibilityConverter : IValueConverter
+        {
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                bool isChecked = (bool)value;
+                return isChecked ? Visibility.Visible : Visibility.Collapsed;
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                throw new NotSupportedException();
+            }
+        }
+    }
+}
